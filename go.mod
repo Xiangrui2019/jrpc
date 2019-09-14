b4ba07b448bd201fea0353c85cf109fc77297028
Go Go Go <xiangrui@aiursoft.com>
2019-09-14 20:24:49 +0800

    add useful

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module jrpc
 go 1.13
 
 require (
+	github.com/gin-contrib/cors v1.3.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/joho/godotenv v1.3.0
 )
--- routers/router.go
+++ routers/router.go
@@ -2,12 +2,16 @@ package routers
 
 import (
 	"jrpc/api"
+	"jrpc/middlewares"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(middlewares.Cors(os.Getenv("CORS_DOMAIN")))
 
 	v1 := router.Group("/jrpc/api/v1")
 	{
