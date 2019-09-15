e94797286eaa2cfb72859d9be1f81afcc9374171
Go Go Go <xiangrui@aiursoft.com>
2019-09-15 10:43:35 +0800

    JRPC Support Dogego Module Dev mode

--- middlewares/sentry.go
+++ middlewares/sentry.go
@@ -1,15 +1,11 @@
 package middlewares
 
 import (
-	"github.com/getsentry/raven-go"
+	"jrpc/modules"
 	"github.com/gin-contrib/sentry"
 	"github.com/gin-gonic/gin"
 )
 
-var SentryClient *raven.Client
-
-func SentryReportor(dsn string) gin.HandlerFunc {
-	SentryClient, _ := raven.New(dsn)
-
-	return sentry.Recovery(SentryClient, true)
+func SentryReportor() gin.HandlerFunc {
+	return sentry.Recovery(modules.SentryClient, true)
 }
